7d22189c0cb702ebc38c23d3b7e4fbe6095f646c
niuyan <632176758@qq.com>
2017-03-20 21:22:19 +0800

    CMP 关键路径算法

--- GFBJ/Graph/Graph/TopoLogicalSort.cpp
+++ GFBJ/Graph/Graph/TopoLogicalSort.cpp
@@ -14,14 +14,15 @@ using namespace std;
  拓扑排序
  
  此方法从上往下遍历，从上往下输出，也可以使用DFS，从下往上遍历
+ sorted是排序后的数组
  */
-void TopSort(AGraph *G)
+int TopSort(AGraph *G, int sorted[])
 {
     ArcNode *p;
     int stack[maxSize], top = -1;
     int i,j;
-    
-    //将入度为0的顶点入栈，等一个顶点和后面的链完成后，换另一个顶点
+    int n;      //计数，判断是否拓扑排序成功
+    //将入度为0的顶点入栈，等一个起始点和后面的孩子们完成后，换其他起始点，保证图中所有顶点全部遍历
     for (i = 1; i <= G->n; ++i)
     {
         if (G->adjList[i].inCount == 0) {
@@ -33,6 +34,8 @@ void TopSort(AGraph *G)
     while (top != -1)
     {
         i = stack[top--];
+        sorted[n] = i;
+        ++n;
         cout<<i<<" ";
         p = G->adjList[i].firstArc;
         //对刚出栈的顶点i的孩子进行入栈操作（若入度为0）
@@ -48,5 +51,11 @@ void TopSort(AGraph *G)
             }
             p = p->nextArc;
         }
+    }
+    //判断n是否与图中顶点个数相等，相等则排序成功，返回1
+    if (n == G->n) {
+        return 1;
+    }else {
+        return 0;
     }
 }
