accfe6c52c87bdd65a2c4e44b740bfa5be4ce971
niuyan <632176758@qq.com>
2017-04-04 19:55:14 +0800

    修改prim算法

--- GFBJ/Graph/Graph/Prim.cpp
+++ GFBJ/Graph/Graph/Prim.cpp
@@ -17,7 +17,7 @@ using namespace std;
  @param G 图
  @param v0 开始顶点编号
  */
-void Prim(MGraph G, int v0)
+int Prim(MGraph G, int v0)
 {
     //存放当前生成树到剩余顶点最短边的权值，0表示已加入树
     //lowcost[i]表示已i为终点的边的最小权值
@@ -25,14 +25,15 @@ void Prim(MGraph G, int v0)
     //mst存放lowcost值中对应边权值的结点，0表示已加入树
     // <mst[i],i>就是一条边，lowcost[i]表示该边的权值
     int mst[maxSize];
+    int sum = 0;
     
-    //初始化lowset和vset数组
+    //初始化lowset和mst数组
     for (int i = 1; i < G.n; ++i)
     {
         lowcost[i] = G.edges[v0][i];    //初始化邻接矩阵中v0那行
         mst[i] = 1;
     }
-    mst[v0] = 1;           //将v0并入树中
+    mst[v0] = 0;           //将v0并入树中
     
     int min;
     //循环完成一次，代表最小生成树并入了一个顶点，n个结点，即n-1条边，遍历次数为n-1
@@ -49,9 +50,11 @@ void Prim(MGraph G, int v0)
                 k = j;
             }
         }
-        cout << "V" << mst[k] << "-V" << k << "=" << min << endl;
         lowcost[k] = 0;
-        
+        mst[k] = 0;
+        sum += min;
+        //输出刚加入最小生成树的边
+        cout << "V" << mst[k] << "-V" << k << "=" << min << endl;
         //以刚并入生成树中的顶点为媒介，更新lowcost
         for (int j = 1; j<= G.n; ++j)
         {
@@ -62,4 +65,33 @@ void Prim(MGraph G, int v0)
             }
         }
     }
+    return sum;
+}
+
+int main()
+{
+    int i, j, k, m, n;
+    int cost;
+    cin >> m >> n;//m=顶点的个数，n=边的个数
+    //初始化图G
+    MGraph *G = new MGraph;
+    for (i = 1; i <= m; i++)
+    {
+        for (j = 1; j <= m; j++)
+        {
+            G->edges[i][j] = INF;
+        }
+    }
+    //构建图G
+    for (k = 1; k <= n; k++)
+    {
+        cin >> i >> j >> cost;
+        G->edges[i][j] = cost;
+        G->edges[j][i] = cost;
+    }
+    //求解最小生成树
+    cost = Prim(*G, m);
+    //输出最小权值和
+    cout << "最小权值和=" << cost << endl;
+    return 0;
 }
